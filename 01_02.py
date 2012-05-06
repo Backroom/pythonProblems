64d756f36ef6e5b5993b996ee0226c235422f1d3
Backroom <karpov.kirov@gmail.com>
2012-05-06 16:38:16 +0400

    Добавление в репозиторий всех решенных на данный момент задач без исправлений, добавлений, комментариев

---
+++ 01_02.py
@@ -0,0 +1,25 @@
+__author__ = 'slaer'
+L = (4, 23, 23, 12, 16, 56, 18, 13, 23, 24, 17, 34, 12, 34, 5, 6, 23, 23, 81, 14)
+count,maxCount,maxNumber,i,k,x,number = 0,0,0,0,0,0,[]
+
+L1 = list(L)
+L1.sort()
+
+print L1
+for i in L1:
+    if i == x:
+        number.append(i)
+    x = i
+
+for i in L1:
+    if i in number:
+        count += 1
+        if count < maxCount:
+            pass
+        else:
+            maxCount = count
+            maxNumber = i
+    else:
+        count = 0
+
+print {'Number': maxNumber, 'Counts': maxCount}
