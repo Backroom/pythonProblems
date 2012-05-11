ae07adaaa3de0a1d522a8e9d0fd55330de5a6c1b
slaer <slaer93@gmail.com>
2012-05-11 14:19:56 +0400

    Не адекватно как то работает

--- 02_03.py
+++ 02_03.py
@@ -3,8 +3,8 @@ __author__ = 'slaer'
 import random
 marksStudents = []
 mark = 0
-taskDict = {str(key): random.choice('abcdef') for key in range(1,21)}
-answerDict = {str(key): random.choice('abcdef') for key in range(1,21)}
+taskDict = {key: random.choice('abcdef') for key in range(1,21)}
+answerDict = {key: random.choice('abcdef') for key in range(1,21)}
 for k, v in taskDict.iteritems():
     if (k in answerDict.keys()) and (v in answerDict.values()):
         marksStudents.append((k,v))
