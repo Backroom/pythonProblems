0b4eac52e63f1107cf0e79d17ed74aebdefbce85
Backroom <karpov.kirov@gmail.com>
2012-05-17 01:19:11 +0400

    Принят TODO, оформлен код, добавлен комментарий

--- 01_03.py
+++ 01_03.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 __author__ = 'slaer'
-M = (0,0,1,0,1,0,0)
+M = (0, 0, 1, 0, 1, 0, 0)
 L = (17, 40, 65, 24, 12, 13, 42)
 #Result L1 = (0, 0, 65, 0, 12, 0, 0)#
-MM,LL = list(M),list(L)
-L1,c,k = [],L[0],0
+MM, LL = list(M), list(L)
+L1, c, k = [], L[0], 0
 if len(MM) != len(LL):              #!!!Добавил обработку такой ситуации, когда длины кортежей не равны меду собой
     print 'turples dont equals'
 else:
@@ -15,7 +15,8 @@ else:
         elif k == 5:
             c = LL[4]
         else: c = L[0]
-        L1.append(i*c)
+        L1.append(i*c)              # странный, коннено, код — обычно проще переписать,
+                                    # но пусть будет для разбора
 print 'First list: ', MM
 print 'Second list: ', LL
 print 'Result: ', L1
@@ -30,12 +31,10 @@ def setMaskToTuple(tupleIn, mask):
         else:
             resultList.append(tupleIn[i])
 
-
     resultTuple = tuple(resultList)
     return resultTuple
 
 # Опять-таки смотрите формулировку задачи — на вход два кортежа и один на выходе
-# TODO: добавить обработку такой ситуации, когда длины кортежей не равны меду собой
 # TODO: напомнить мне[Karpov], что бы я показал немного черной магии с этим примером
 
 
